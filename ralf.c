2750e709628f9ed8b8bbae5e9d0d37d47bb53351
bmcphee <brennen.mcphee@gmail.com>
2016-03-29 16:11:47 -0600

    Initial functions and descriptions

---
+++ ralf.c
@@ -0,0 +1,29 @@
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]){
+	if(argc != 2){
+		printf("Error: please provide an argument);
+		exit(1);
+	}
+	
+	if(displayDir == 1){
+		
+}
+
+/** 
+ * @brief Displays the permissions, number of hard-links, owner,
+ * 				group
+ */
+int displayFileInfo(char *filename){
+	
+}
+
+int displayDir(char *dir({
+
+}
